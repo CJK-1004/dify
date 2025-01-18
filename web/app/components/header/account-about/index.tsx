3060b05619df21ac387fb4315cc452c30fc0a469
cjk <magic@magice.co>
2025-01-18 19:23:47 +0900

    update

--- web/app/components/header/account-about/index.tsx
+++ web/app/components/header/account-about/index.tsx
@@ -23,7 +23,6 @@ export default function AccountAbout({
   onCancel,
 }: IAccountSettingProps) {
   const { t } = useTranslation()
-  const isLatest = langeniusVersionInfo.current_version === langeniusVersionInfo.latest_version
 
   return (
     <Modal
@@ -37,7 +36,7 @@ export default function AccountAbout({
         </div>
         <div>
           <LogoSite className='mx-auto mb-2' />
-          <div className='mb-3 text-center text-xs font-normal text-gray-500'>Version {langeniusVersionInfo?.current_version}</div>
+          <div className='mb-3 text-center text-xs font-normal text-gray-500'></div>
           <div className='mb-4 text-center text-xs font-normal text-gray-700'>
             <div>© {dayjs().year()} MagicEcole, Inc.</div>
             {/*}
@@ -56,13 +55,6 @@ export default function AccountAbout({
         </div>
         <div className='mb-4 -mx-8 h-[0.5px] bg-gray-200' />
         <div className='flex justify-between items-center'>
-          <div className='text-xs font-medium text-gray-800'>
-            {
-              isLatest
-                ? t('common.about.latestAvailable', { version: langeniusVersionInfo.latest_version })
-                : t('common.about.nowAvailable', { version: langeniusVersionInfo.latest_version })
-            }
-          </div>
           {/*
           <div className='flex items-center'>
             <Link
